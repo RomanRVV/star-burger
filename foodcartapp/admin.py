4cc4a7b3656b0d911dc50a9936f2a70a2e7fc7e1
RomanRVV <rvachyov.roma@yandex.ru>
2023-10-15 16:21:48 +0300

    add response change func

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
 
 from .models import Product
 from .models import ProductCategory
@@ -121,3 +125,15 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline
     ]
+
+    def response_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        next_url = request.GET.get('next')
+
+        if next_url:
+            allowed_hosts = settings.ALLOWED_HOSTS
+
+            if url_has_allowed_host_and_scheme(next_url, allowed_hosts):
+                return HttpResponseRedirect(next_url)
+
+        return res
