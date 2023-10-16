e0b126cdae056e3c3fae020862f63ab511843958
RomanRVV <rvachyov.roma@yandex.ru>
2023-10-16 16:53:19 +0300

    add a new app

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import views as auth_views
 
 
 from foodcartapp.models import Product, Restaurant, Order
-
+from places.models import Place
 from geopy.distance import geodesic
 from geopy import distance
 from environs import Env
@@ -100,22 +100,31 @@ def view_restaurants(request):
 
 def fetch_coordinates(address):
 
-    apikey = settings.YANDEX_API_KEY
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-    if not found_places:
-        return None
+    place = Place.objects.filter(address=address).first()
 
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    if place and place.lat and place.lon:
 
-    return float(lon), float(lat)
+        return float(place.lon), float(place.lat)
+    else:
+
+        apikey = settings.YANDEX_API_KEY
+        base_url = "https://geocode-maps.yandex.ru/1.x"
+        response = requests.get(base_url, params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        })
+        response.raise_for_status()
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+        if not found_places:
+            return None
+
+        most_relevant = found_places[0]
+        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+
+        Place.objects.create(address=address, lat=lat, lon=lon)
+
+        return float(lon), float(lat)
 
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
