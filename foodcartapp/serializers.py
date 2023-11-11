0af2a887b56b79cbb44c82b8d2512339e96f486a
RomanRVV <rvachyov.roma@yandex.ru>
2023-11-11 14:19:08 +0300

    configured the serializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,27 +1,13 @@
-from django.core.exceptions import ObjectDoesNotExist
-
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import CharField
 from rest_framework.serializers import ListField
-from rest_framework.serializers import ValidationError
 
-from .models import Product
 from .models import Order
 from .models import OrderItem
 
+from phonenumber_field.serializerfields import PhoneNumberField
+
 
 class OrderItemSerializer(ModelSerializer):
-
-    product = CharField()
-
-    def validate_product(self, value):
-        try:
-            Product.objects.get(id=value)
-            return value
-        except ObjectDoesNotExist:
-            raise ValidationError(
-                f'products: Недопустимый первичный ключ {value}'
-            )
 
     class Meta:
         model = OrderItem
@@ -33,6 +19,7 @@ class OrderSerializer(ModelSerializer):
     products = ListField(
         child=OrderItemSerializer(), allow_empty=False, write_only=True
     )
+    phonenumber = PhoneNumberField()
 
     class Meta:
         model = Order
